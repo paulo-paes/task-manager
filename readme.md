1e006fbcbf070e5f7d99fbc3da3d669e72eb8da0
Paulo Paes <paulo.paes@wises.com.br>
2023-01-13 20:22:55 -0300

    chore: initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Task Manager
+
+A project to handle simple daily tasks
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>task manager</div> 
+  )
+}
+
+export default App
